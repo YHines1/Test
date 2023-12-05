905a8264beffec30f693ef52286210af8abfe3ab
YHines1 <148097405+YHines1@users.noreply.github.com>
2023-12-05 00:34:38 -0600

    Update user-prompts.sh

--- .devcontainer/user-prompts.sh
+++ .devcontainer/user-prompts.sh
@@ -6,3 +6,14 @@ export PROJECT_NAME=$project_name
 
 # Output gathered information
 echo "Project Name: $PROJECT_NAME"
+
+# Prompt user for Azure DevOps repository URL
+read -p "Enter the Azure DevOps repository URL: " repo_url
+
+# Prompt user for Azure DevOps Personal Access Token (PAT)
+read -p "Enter your Azure DevOps Personal Access Token (PAT): " pat
+
+mkdir /workspace 
+
+# Clone the repository
+git clone $repo_url /workspace
