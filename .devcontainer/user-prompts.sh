e168d81d89a95178bb7f71877e9e92458d198ed6
YHines1 <148097405+YHines1@users.noreply.github.com>
2023-12-04 22:33:20 -0600

    Update user-prompts.sh

--- .devcontainer/user-prompts.sh
+++ .devcontainer/user-prompts.sh
@@ -3,11 +3,6 @@
 # Prompt user for project name
 read -p "Enter project name: " projectName
 
-# Prompt user for programming language
-read -p "Enter programming language: " programmingLanguage
-
-# Prompt user for additional tools and dependencies
-read -p "Enter additional tools and dependencies (comma-separated): " additionalTools
 
 # Create a directory with the project name
 mkdir -p /workspace/$projectName
@@ -17,11 +12,6 @@ cd /workspace/$projectName
 
 # Output information about the selected options
 echo "Project Name: $projectName"
-echo "Programming Language: $programmingLanguage"
-echo "Additional Tools and Dependencies: $additionalTools"
-
-# Now, you can perform any additional setup based on user input
-# For example, install specific language or framework dependencies
 
 # Continue with the original entry point (e.g., start a shell)
 exec "$@"
