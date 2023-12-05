fac93a4d9edbf56b878b9942f588b95235333e72
YHines1 <148097405+YHines1@users.noreply.github.com>
2023-12-04 23:02:09 -0600

    Update user-prompts.sh

--- .devcontainer/user-prompts.sh
+++ .devcontainer/user-prompts.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 # Prompt user for project name
-read -p "Enter project name: " projectName
+read -p "Enter the name of the project: " project_name
+export PROJECT_NAME=$project_name
 
-
-# Create a directory with the project name
-mkdir -p /workspace/$projectName
-
-# Change into the project directory
-cd /workspace/$projectName
-
-# Output information about the selected options
-echo "Project Name: $projectName"
-
-# Continue with the original entry point (e.g., start a shell)
-exec "$@"
+# Output gathered information
+echo "Project Name: $PROJECT_NAME"
